94ce40e6bcdddc3b29320da7a44767cd713672b0
JDarwind <boscardingiacomo@gmail.com>
2022-11-24 13:24:35 +0100

    Added Factory Support
    Added Factory to the library to help in the process of Manager Instantiation.

--- src/Factory.php
+++ src/Factory.php
@@ -3,12 +3,30 @@
 namespace Jdarwind\PortableConfigurationManager;
 class Factory
 {
-    public static function make($type): ManagerInterface|null{
+    /**
+     * @throws \Exception
+     */
+    public static function make($type, array $args = []): ManagerInterface|null{
         $instance = null;
         $namespace = __NAMESPACE__;
         $instanceNamespace = sprintf('%s\\%s\Manager', $namespace, $type);
         if(class_exists($instanceNamespace)) {
-            $instance = new $instanceNamespace;
+            $reflection = new \ReflectionClass($instanceNamespace);
+            $instance = $reflection->newInstanceArgs($args);
+        }
+        return $instance;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public static function makeImmutable($type, array $args = []): ManagerInterface|null{
+        $instance = null;
+        $namespace = __NAMESPACE__;
+        $instanceNamespace = sprintf('%s\\%s\Immutable\Manager', $namespace, $type);
+        if(class_exists($instanceNamespace)) {
+            $reflection = new \ReflectionClass($instanceNamespace);
+            $instance = $reflection->newInstanceArgs($args);
         }
         return $instance;
     }
